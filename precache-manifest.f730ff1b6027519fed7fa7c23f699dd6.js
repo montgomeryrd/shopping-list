6c749930e9277023b0edcd908b65112a4cd00a5b
montgomeryrd <montgomery_rd@yahoo.com>
2020-06-04 23:44:36 -0700

    Updates

---
+++ precache-manifest.f730ff1b6027519fed7fa7c23f699dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e63d0d431c2a94c39a9ac608ba9e442",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "a078c91d99e7c5f10004",
+    "url": "/shopping-list/static/css/main.b0baaf90.chunk.css"
+  },
+  {
+    "revision": "4d48d9861a4142255ad7",
+    "url": "/shopping-list/static/js/2.02a083b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-list/static/js/2.02a083b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a078c91d99e7c5f10004",
+    "url": "/shopping-list/static/js/main.79df015e.chunk.js"
+  },
+  {
+    "revision": "53fbdae1ab7d4ead5d4c",
+    "url": "/shopping-list/static/js/runtime-main.f922e5fb.js"
+  }
+]);
